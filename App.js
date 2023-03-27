82dcaf7943626e04a9dff21e468072d502d765ef
Emanuel Silva <emanuel.silva@grupofm.local>
2023-03-27 11:58:08 -0300

    projeto mobile

--- App.js
+++ App.js
@@ -1,39 +1,38 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { HomeScreen } from './src/components/Home/HomeScreen';
-import { ProfileScreen } from './src/components/Profile/ProfileScreen';
-import { SignUpScreen } from './src/components/SignUp/signUp';
-import { SignInScreen } from './src/components/SingIn/SignIn';
+import { HomeScreen } from './src/components/screens/HomeScreen';
+import { ProfileScreen } from './src/components/screens/ProfileScreen';
+import { SignUpScreen } from './src/components/screens/signUp';
+import { SignInScreen } from './src/components/screens/SignIn';
 
 import { initializeApp } from 'firebase/app';
 
 const Stack = createStackNavigator();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX48nDF7cml6wcB4lPz3TxsG_55dQOBUo",
-  authDomain: "oxelab-team.firebaseapp.com",
-  projectId: "oxelab-team",
-  storageBucket: "oxelab-team.appspot.com",
-  messagingSenderId: "86784909294",
-  appId: "1:86784909294:web:6a8c682a314cf4d12878ff"
+  apiKey: "AIzaSyDczh-dXCNlBIW00Yjvi4pZAOWpEUB-u6M",
+  authDomain: "firabase-auth-f03dd.firebaseapp.com",
+  projectId: "firabase-auth-f03dd",
+  storageBucket: "firabase-auth-f03dd.appspot.com",
+  messagingSenderId: "862118169832",
+  appId: "1:862118169832:web:0a5449217990e4a3dbcd5d"
+
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name='Profile' component={ProfileScreen} />
-        <Stack.Screen name='SignUp' component={SignUpScreen} />
-        <Stack.Screen name='SignIn' component={SignInScreen} />
+        <Stack.Screen options={{ headerShown: false }} name='Home' component={HomeScreen} />
+        <Stack.Screen options={{ headerShown: false }} name='Profile' component={ProfileScreen} />
+        <Stack.Screen options={{ headerShown: false }} name='SignUp' component={SignUpScreen} />
+        <Stack.Screen options={{ headerShown: false }} name='SignIn' component={SignInScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
